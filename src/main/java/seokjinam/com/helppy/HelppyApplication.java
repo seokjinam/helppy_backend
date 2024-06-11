50206893de32f6a909e9fa10429b6604bcb7b7df
Balloon208 <gnswldms2@gmail.com>
2024-06-11 09:08:16 +0900

    init

---
+++ src/main/java/seokjinam/com/helppy/HelppyApplication.java
@@ -0,0 +1,13 @@
+package seokjinam.com.helppy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelppyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelppyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/seokjinam/com/helppy/HelppyApplicationTests.java
@@ -0,0 +1,13 @@
+package seokjinam.com.helppy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelppyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
